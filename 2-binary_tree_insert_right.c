323cf3e164246bbfaf3fb6122104c7a21f6737a4
Abu Jehoiada <abujehoiada@gmail.com>
2024-06-04 16:47:23 -0700

    Fixed Task 2 (Insert right)

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -1,32 +1,29 @@
 #include "binary_trees.h"
 
 /**
- * binary_tree_insert_left - Inserts a node as the left-child of another node
+ * binary_tree_insert_right - Inserts a node as the right-child of another node
  * @parent: pointer to the node to insert the left-child in
  * @value: value to store in the new node
  * Return: Null or pointer to new node
  */
-binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
+binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_left = malloc(sizeof(binary_tree_t));
+	binary_tree_t *new_right;
 
-	if (parent == NULL || new_left == NULL)
+	if (parent == NULL)
 		return (NULL);
 
-	new_left->n = value;
-	new_left->parent = parent;
-	new_left->left = NULL;
-	new_left->right = NULL;
+	new_right = binary_tree_node(parent, value);
+	if (new_right == NULL)
+		return (NULL);
 
-	if (parent->left != NULL)
+	if (parent->right != NULL)
 	{
-		new_left->left = parent->left;
-		parent->left->parent = new_left;
-		parent->left = new_left;
+		new_right->right = parent->right;
+		parent->right->parent = new_right;
 	}
-	else
-		parent->left = new_left;
+	parent->right = new_right;
 
-	return (new_left);
+	return (new_right);
 
 }
