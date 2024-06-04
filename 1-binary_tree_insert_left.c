04ae1ed897cb6baa47c70fd26bf10c98ab00cbed
Abu Jehoiada <abujehoiada@gmail.com>
2024-06-04 16:40:36 -0700

    Fixed task 1

--- 1-binary_tree_insert_left.c
+++ 1-binary_tree_insert_left.c
@@ -8,25 +8,21 @@
  */
 binary_tree_t *binary_tree_insert_left(binary_tree_t *parent, int value)
 {
-	binary_tree_t *new_left = binary_tree_node(parent, value);
+	binary_tree_t *new_left;
 
-	if (parent == NULL || new_left == NULL)
+	if (parent == NULL)
 		return (NULL);
 
-/*	
-	new_left->n = value;
-	new_left->parent = parent;
-	new_left->left = NULL;
-	new_left->right = NULL;
-*/
+	new_left = binary_tree_node(parent, value);
+	if (new_left == NULL)
+		return (NULL);
+
 	if (parent->left != NULL)
 	{
 		new_left->left = parent->left;
 		parent->left->parent = new_left;
-		parent->left = new_left;
 	}
-	else
-		parent->left = new_left;
+	parent->left = new_left;
 
 	return (new_left);
 
