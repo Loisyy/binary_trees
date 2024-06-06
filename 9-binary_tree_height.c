1ae38e8901d44ff210a0a6b58815084e9c356848
Abu Jehoiada <abujehoiada@gmail.com>
2024-06-05 19:52:05 -0700

    Task 10 (Tree Depth)

--- 9-binary_tree_height.c
+++ 9-binary_tree_height.c
@@ -12,7 +12,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	size_t left_height = 0, right_height = 0, max_height = 0;
 
 	if (tree == NULL)
-                return (0);
+		return (0);
 	if (tree->left != NULL)
 	{
 		left_height = binary_tree_height(tree->left) + 1;
@@ -20,6 +20,6 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree->right != NULL)
 		right_height = binary_tree_height(tree->right) + 1;
 	max_height = MAX(left_height, right_height);
-	return max_height;
+	return (max_height);
 
 }
